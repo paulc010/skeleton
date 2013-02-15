e52bca683a6ee811a4b6e445bbaaccaec4a8d15b
Paul Campbell <pcampbell@ecartservice.net>
2013-02-15 09:51:05 -0800

    License update to afl-3.0

--- skeleton/skeleton.php
+++ skeleton/skeleton.php
@@ -1,4 +1,19 @@
 <?php
+/*
+* 2007-2012 eCartService.net
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+*
+*  @author eCartService.net <pcampbell@ecartservice.net>
+*  @copyright  2009-2013 eCartService.net
+*  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*
+*/
 
 if (!defined('_PS_VERSION_'))
 	exit;
